e3ebbadb25b5123a63964d433fe50020347ad13c
Raul <raulmayas20@gmail.com>
2021-07-18 17:42:22 -0500

    Creating NL Map for Ginis Visualization

---
+++ gini_map/static/js/logic.js
@@ -0,0 +1,67 @@
+function getColor(d) {
+  return d > 600  ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 400  ? '#E31A1C' :
+         d > 300  ? '#FC4E2A' :
+         d > 200  ? '#FD8D3C' :
+         d > 100  ? '#FEB24C' :
+         d > -5  ? '#FED976' :
+                    '#FFEDA0';
+}
+
+var nlMunicipality = "static/data/nyu_geojson.json"
+
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
+  accessToken: API_KEY
+  });
+
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+  });
+
+  var outdoormap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+    });
+
+    var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/satellite-v9",
+      accessToken: API_KEY
+      });
+
+      var baseMaps = {
+        "Satellite": satellitemap,
+        "Outdoor": outdoormap,
+        "Light": lightmap,
+        "Dark": darkmap
+    };
+
+  var myMap = L.map("map",{
+    center: [25.02, -100.52],
+    zoom: 7,
+    layers: [darkmap]
+  });
+  
+  L.control.layers(baseMaps).addTo(myMap);
+
+d3.json(nlMunicipality).then(function(data){
+  L.geoJson(data).addTo(myMap);
+});
