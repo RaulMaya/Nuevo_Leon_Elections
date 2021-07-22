87265a87bc4fa721cfc4f2fa301b8339c2355b12
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-07-22 13:50:09 -0500

    Add Multiple ChartJS

--- elections_plotly/static/js/app.js
+++ elections_plotly/static/js/app.js
@@ -1,14 +1,32 @@
+// DEFAULT ID
+var defaultID = 19001;
+
+// ENDPOINTS
+var link_id = "../../../cleaning/output/id_municipios.json";
+var link_a2015 = "../../../cleaning/output/resultados_a2015.json";
+var link_a2018 = "../../../cleaning/output/resultados_a2018.json";
+var link_a2021 = "../../../cleaning/output/resultados_a2021.json";
+var link;
+
 // SELECT HTML ELEMENTS
+var title = d3.select("#nombre-municipio");
 var dropdown = d3.select("#selMunicipio");
 var resultsInfo = d3.select("#election-results");
-
-
 
 // RENDER DROPDOWN FUNCTION
 function renderDrowpdown(municipios, ids) {
     for (var i = 0; i < municipios.length; i++) {
         dropdown.append('option').text(municipios[i]).attr("value", (ids[i]));
     }
+}
+
+// RENDER TITLE FUNCTION
+function renderTitle(id_municipio) {
+
+    d3.json(link_id).then((id_data) => {
+        var rowMunicipio = id_data.find((d) => d.ID === id_municipio);
+        title.html("").append('p').text(`Municipio ${rowMunicipio.ID}: ${rowMunicipio.Municipio}`);
+    });
 }
 
 // RENDER RESULTS FUNCTION
@@ -19,83 +37,169 @@ function renderResults(municipio) {
         .append('p').text(`Ganador: ${ganador}`)
         .append('p').text(`Votos: ${municipio[ganador]}`)
         .append('p').text(`${Math.round((municipio[ganador] / municipio.Total * 100), 2)}%`)
-
     //         .style('font-size', '11.5px')
     //         .style('font-weight', 'bold');
 }
 
+// RENDER CHART
+// function renderChart(x, y, id, year) {
+
+//     var color = ['blue', 'red', 'orange', 'brown', 'gray'];
+//     var trace = { type: 'bar', orientation: 'h', x: x.reverse(), y: y.reverse(),
+//         marker: { color: color.reverse() } };
+//     var layout = { title: {text: year, font: { size: 20, color: 'black' }, y: 0.87},
+//         xaxis: {title: {text: "Votación", standoff: 15} } };
+
+//     Plotly.newPlot(id, [trace], layout);
+
+// }
+
+// RENDER CHART.JS
+function renderChartJS(x, y, year) {
+
+    var color = ['blue', 'red', 'orange', 'brown', 'gray'];
+
+    var chart2015 = document.getElementById("chart-2015").getContext('2d');
+    var chart2018 = document.getElementById("chart-2018").getContext('2d');
+    var chart2021 = document.getElementById("chart-2021").getContext('2d');
+
+    if (year === 2015) { myChart = chart2015; }
+    else if (year === 2018) { myChart = chart2018; }
+    else if (year === 2021) { myChart = chart2021; }
+    
+    let hBar = new Chart(myChart, {
+            type:'bar',
+            data:{
+                labels: y,
+                datasets: [{
+                    label: year,
+                    data: x,
+                    backgroundColor: color
+                }],
+            },
+            options:{indexAxis: 'y'}
+        });
+}
+
+// RENDER YEAR
+function renderYear(id_municipio, year) {
+
+    if (year === 2015) { link = link_a2015; text = "2015"; id = "chart-2015" }
+    else if (year === 2018) { link = link_a2018; text = "2018"; id = "chart-2018" }
+    else if (year === 2021) { link = link_a2021; text = "2021"; id = "chart-2021" }
+
+    d3.json(link).then((year_data) => {
+
+        var rowMunicipio = year_data.find((d) => d.ID === id_municipio);
+        var partidos = ['PAN', 'PRI', 'MC', 'MORENA', 'Otro'];
+        var x = partidos.map(d => rowMunicipio[d]);
+        var y = partidos.map(d => `${d} `);
+
+        // renderChart(x, y, text, id)
+
+        renderChartJS(x, y, year)
+
+        console.log("Municipio:", rowMunicipio.Municipio);
+        console.log("Año:", year);
+        console.log("X:", x);
+        console.log("Y:", y);
+
+    });
+}
+
+
+
+// RUN FUNCTION
+function runEvent(id_municipio) {
+
+    // DROPDOWN (delete)
+    d3.json(link_id).then((id_data) => {
+        console.log("Municipios:", id_data);
+        var ids = id_data.map(d => d.ID);
+        var municipios = id_data.map(d => d.Municipio);
+        console.log("IDs:", ids);
+        console.log("Municipios:", municipios);
+        renderDrowpdown(municipios, ids);
+    });
+
+    renderTitle(id_municipio);
+    renderYear(id_municipio, 2015);
+    renderYear(id_municipio, 2018);
+    renderYear(id_municipio, 2021);
+
+
+}
+
+
+// INIT
+runEvent(defaultID)
 
 
 
 
+
+/*
 
 // LOAD, RENDER & PLOT
 // Read JSON dataset using D3 library (samples.json)
-d3.json("../../../cleaning/Elecciones_NL/output/id_municipios.json").then((data) => {
-
-    console.log("Municipios:", data);
-
-    var ids = data.map(d => d.ID);
-    var municipios = data.map(d => d.Municipio);
-
-    console.log("IDs:", ids);
-    console.log("Municipios:", municipios);
-
-    renderDrowpdown(municipios, ids);
 
 
-    d3.json("../../../cleaning/Elecciones_NL/output/resultados_a2021.json").then((a2021) => {
 
-        console.log("Resultados:", a2021)
+d3.json("../../../cleaning/output/resultados_a2021.json").then((a2021) => {
 
-        var defaultMunicipio = a2021.find((d) => d.ID === 19021);
+    console.log("Resultados:", a2021)
 
-        console.log("defaultMunicipio:", defaultMunicipio)
+    var defaultMunicipio = a2021.find((d) => d.ID === defaultID);
+    console.log("defaultMunicipio:", defaultMunicipio)
 
-        renderResults(defaultMunicipio);
+    renderTitle(defaultMunicipio);
 
-        var ganadores = ['PAN', 'PRI', 'MC', 'MORENA', 'PANAL', 'PES', 'PRD'];
-        var color = ['blue', 'red', 'orange', 'brown', 'pink', 'cyan', 'yellow']
-        var x = ganadores.map(d => defaultMunicipio[d] / defaultMunicipio.Total * 100).reverse();
-        var y = ganadores.map(d => `${d} `).reverse();
 
-        // HORIZONTAL BAR CHART
-        var trace = {
-            type: 'bar',
-            orientation: 'h',
-            x: x,
-            y: y,
-            // text: otu_labels.slice(0, 10)
-            marker: {
-                color: color.reverse()
-            }
-        };
 
-        var layout = {
+    renderResults(defaultMunicipio);
+
+    var ganadores = ['PAN', 'PRI', 'MC', 'MORENA', 'Otro'];
+    var color = ['blue', 'red', 'orange', 'brown', 'gray'];
+    var x = ganadores.map(d => defaultMunicipio[d]).reverse();
+    var y = ganadores.map(d => `${d} `).reverse();
+
+    // HORIZONTAL BAR CHART
+    var trace = {
+        type: 'bar',
+        orientation: 'h',
+        x: x,
+        y: y,
+        // text: otu_labels.slice(0, 10)
+        marker: {
+            color: color.reverse()
+        }
+    };
+
+    var layout = {
+        title: {
+            text: "2015",
+            font: { size: 20, color: 'black' },
+            y: 0.87
+        },
+        xaxis: {
             title: {
-                text: "Resultados Electorales",
-                font: { size: 20, color: 'black' },
-                y: 0.87
-            },
-            xaxis: {
-                title: {
-                    text: "Porcentaje Votación",
-                    standoff: 15
-                }
+                text: "Votación",
+                standoff: 15
             }
-        };
+        }
+    };
 
-        Plotly.newPlot('bar', [trace], layout);
-
-
-
+    Plotly.newPlot('chart-2015', [trace], layout);
 
 
 
 
 
 
-    })
+
+
+
+})
 
 
 
