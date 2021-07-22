11a7fd60992313589e20fb4aff18a11600f34a6a
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-07-22 17:43:16 -0500

    Add Participation Bar

--- elections_plotly/static/js/app.js
+++ elections_plotly/static/js/app.js
@@ -41,7 +41,7 @@ function renderResults(municipio) {
     //         .style('font-weight', 'bold');
 }
 
-// RENDER CHART.JS
+// RENDER CHART.JS FUNCTION
 function renderChartJS(x, y, year) {
 
     // Pending: choose pretty colors & alpha
@@ -51,6 +51,7 @@ function renderChartJS(x, y, year) {
     var chart2018 = document.getElementById("chart-2018").getContext('2d');
     var chart2021 = document.getElementById("chart-2021").getContext('2d');
 
+    // Pending: Set animation parameters
     params = {
         type: 'bar',
         data: {
@@ -63,22 +64,58 @@ function renderChartJS(x, y, year) {
         options: {
             indexAxis: 'y',
             plugins: {
-                title: {display: true, text: year, font: { size: 25 }},
+                title: { display: true, text: year, font: { size: 25 } },
                 legend: { display: false }
             }
         }
     }
 
-
-if (year === 2015) { results2015 = new Chart(chart2015, params); }
-else if (year === 2018) { results2018 = new Chart(chart2018, params); }
-else if (year === 2021) { results2021 = new Chart(chart2021, params); }
-
+    if (year === 2015) { results2015 = new Chart(chart2015, params); }
+    else if (year === 2018) { results2018 = new Chart(chart2018, params); }
+    else if (year === 2021) { results2021 = new Chart(chart2021, params); }
 
 }
 
-// RENDER YEAR
-function renderYear(id_municipio, year) {
+
+// RENDER BAR.JS FUNCTION
+function renderBarJS(partMunicipio, partNL, year) {
+
+    var bar2015 = document.getElementById("bar-2015").getContext('2d');
+    var bar2018 = document.getElementById("bar-2018").getContext('2d');
+    var bar2021 = document.getElementById("bar-2021").getContext('2d');
+
+    // Pending: Set animation parameters
+    params = {
+        type: 'bar',
+        data: {
+            labels: ['Municipio'],
+            datasets: [{
+                data: [partMunicipio],
+                backgroundColor: "yellow",
+                barThickness: 15,
+            }],
+        },
+        options: {
+            indexAxis: 'y',
+            plugins: {
+                title: { display: true, text: "Participación ciudadana", font: { size: 15 } },
+                legend: { display: false }
+            },
+            scales: { x: { min: 0, max: 1 } }
+        }
+    }
+
+    if (year === 2015) { part2015 = new Chart(bar2015, params); }
+    else if (year === 2018) { part2018 = new Chart(bar2018, params); }
+    else if (year === 2021) { part2021 = new Chart(bar2021, params); }
+
+}
+
+
+
+
+// RUN YEAR FUNCTION
+function runYear(id_municipio, year) {
 
     if (year === 2015) { link = link_a2015; text = "2015"; id = "chart-2015" }
     else if (year === 2018) { link = link_a2018; text = "2018"; id = "chart-2018" }
@@ -91,20 +128,26 @@ function renderYear(id_municipio, year) {
         var x = partidos.map(d => rowMunicipio[d]);
         var y = partidos.map(d => `${d} `);
 
-        // renderChart(x, y, text, id)
-
         renderChartJS(x, y, year)
 
-        console.log("Municipio:", rowMunicipio.Municipio);
-        console.log("Año:", year);
-        console.log("X:", x);
-        console.log("Y:", y);
+        var totalVotos = year_data.map(d => d.Total);
+        var listaNominal = year_data.map(d => d.LNominal);
+        var partNL = math.add(...totalVotos) / math.add(...listaNominal)
+        var partMunicipio = rowMunicipio.Total / rowMunicipio.LNominal;
+
+        renderBarJS(partMunicipio, partNL, year);
+
+        console.log("Participación:", partNL, partMunicipio);
+
+        // console.log("Municipio:", rowMunicipio.Municipio);
+        // console.log("Año:", year);
+        // console.log("X:", x);
+        // console.log("Y:", y);
 
     });
 }
 
-
-// RUN FUNCTION
+// RUN EVENT FUNCTION
 function runEvent(id_municipio) {
 
     // DROPDOWN
@@ -119,12 +162,11 @@ function runEvent(id_municipio) {
     });
 
     renderTitle(id_municipio);
-    renderYear(id_municipio, 2015);
-    renderYear(id_municipio, 2018);
-    renderYear(id_municipio, 2021);
+    runYear(id_municipio, 2015);
+    runYear(id_municipio, 2018);
+    runYear(id_municipio, 2021);
 
 }
-
 
 // INIT
 runEvent(defaultID);
@@ -138,9 +180,13 @@ function optionChanged() {
     results2018.destroy();
     results2021.destroy();
 
+    part2015.destroy();
+    part2018.destroy();
+    part2021.destroy();
+
     runEvent(selectedID);
 
 }
 
-
+// Pending: Delete debug MSGs
 
